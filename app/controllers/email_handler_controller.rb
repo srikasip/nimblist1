1c4dc51b6a75b216c9dcc5d22db748d5242459f3
srikasip <srikasip@gmail.com>
2013-10-31 14:09:33 -0400

    Again, i'm adding stuff to straighten out the email acceptance stuff

--- app/controllers/email_handler_controller.rb
+++ app/controllers/email_handler_controller.rb
@@ -31,35 +31,22 @@ class EmailHandlerController < ApplicationController
             tags = base_tags + line_item
             task.create_task(user, item, tags)
           end
-        end
-
-      
+        end      
       end
 
       #if a response or a fwd, then get top level tags and store them with processed item tag
       #if a response is a first time, then parse for top level tags, and then get tags for each item
+
+      MailGun.create(:subject => params['subject'], :sender => params['sender'], :recipient => params['recipient'], :stripped_text => params['stripped-text'], :body_plain => params['body-plain'])
+      
       render :text=>'Success', :status=>:ok
 
     end
-
-
-
-    # Message parsed into broad objects
-
-    #-------------Different Email sending formats--------------#
-    #1. A forwarded email with hash tags
-    #2. A reply to email with hash tags
-    #3. A list of tasks with ONE item (all tags in email refer to the item)
-    #4. A list of tasks with MULTIPLE items
-    #4a. One set of tags for all the items
-    #4b. Each item has its own tags
-
-
-    #Step 1: Get the User
-    #Step 2: Decide on the Email Format
-    #Step 3: Parse the Email
-
   end
+
+
+
+
 
   private
 
