a927c24c7f2ab2e4a7e51b0ab7477f9bc552f911
srikasip <srikasip@gmail.com>
2013-11-03 20:28:37 -0500

    Fixing a small bug in task retrieval that includes tasks across users with the same tag name

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ActiveRecord::Base
   def tasks(active_tag_id)
     if active_tag_id != 0
       all_tasks = Task.select(:task_id).where('user_id=?', self.id)
-      taskTags = TaskTags.select(:task_id).where(:tag_id=>active_tag_id, :task_id=>all_tasks).distinct
-      tasks = Task.where(id: taskTags)
+      taskTags = TaskTags.select(:task_id).where(:tag_id=>active_tag_id).distinct
+      tasks = Task.where(:id=> taskTags, :user_id=>self.id)
     else
       tasks = Task.find_all_by_user_id(self.id)
     end
